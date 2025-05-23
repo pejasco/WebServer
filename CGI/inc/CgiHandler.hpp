69c252a00faa2d217a4455fac0998a6fa815f082
pandashaly <shalycatori@gmail.com>
2025-05-23 19:09:43 +0100

    cgi wrapper

--- CGI/inc/CgiHandler.hpp
+++ CGI/inc/CgiHandler.hpp
@@ -6,7 +6,22 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 20:49:03 by ssottori          #+#    #+#             */
-/*   Updated: 2025/05/17 20:50:13 by ssottori         ###   ########.fr       */
+/*   Updated: 2025/05/23 18:18:02 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <string>
+#include "receiveRequest.hpp"
+#include "executeScript.hpp"
+
+class CgiHandler {
+	private:
+		RequestData _request;
+		std::string _scriptPath;
+
+	public:
+		CgiHandler(const RequestData &request, const std::string &scriptPath);
+		std::string run();
+};
--- CGI/src/CgiHandler.cpp
+++ CGI/src/CgiHandler.cpp
@@ -6,7 +6,16 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 20:48:58 by ssottori          #+#    #+#             */
-/*   Updated: 2025/05/22 16:25:18 by ssottori         ###   ########.fr       */
+/*   Updated: 2025/05/23 18:18:18 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "CgiHandler.hpp"
+
+CgiHandler::CgiHandler(const RequestData &request, const std::string &scriptPath)
+	: _request(request), _scriptPath(scriptPath) {}
+
+std::string CgiHandler::run() {
+	ScriptExecutor executor(_scriptPath, _request);
+	return executor.runScript();
+}
